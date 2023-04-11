f24bea3142d41f982636abd0f2c3ad96cd1b0387
ThanhFlynn <thanhnct244@gmail.com>
2023-04-11 13:35:04 +0700

    django and react

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',views.getRoutes, name="routes"),
     path('notes/',views.getNotes, name="notes"),
     path('notes/<str:pk>',views.getNote, name="note"),
-    path('account/', views.getAccounts, name="accounts"),
-    path('account/<str:pk>', views.getAccount, name="account"),
+    path('accounts/', views.getAccounts, name="accounts"),
+    path('accounts/<str:pk>', views.getAccount, name="account"),
 ]
